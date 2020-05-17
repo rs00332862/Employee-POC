ead05f817644bad2d2e47e01bd5d3ac13bfedb4c
Rohit Shivankar <rs00332862@techmahindra.com>
2020-05-17 22:13:53 +0530

    Added unit test cases for employee list view controller

--- Employee POC/Employee POCTests/Employee_POCTests.swift
+++ Employee POC/Employee POCTests/Employee_POCTests.swift
@@ -10,25 +10,67 @@ import XCTest
 @testable import Employee_POC
 
 class Employee_POCTests: XCTestCase {
-
+    
+    let employeeViewModel = EmployeeViewModel()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        testGetEmployeesDataFromWebService()
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testGetEmployeesDataFromWebService() {
+        let expectation = self.expectation(description: "Web Service response successful.")
+        employeeViewModel.getEmployeeList { (result) in
+            switch(result) {
+            case .success(let result):
+                XCTAssertNotNil(result)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 7, handler: nil)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testEmployeeSearchResultCount() {
+        employeeViewModel.filterEmployeeDataWith(string: "Ce") {
+            XCTAssertEqual(EmployeeViewModel.filteredEmployeesList.count, 2)
         }
     }
-
+    
+    func testEmployeeSearchResultData() {
+        employeeViewModel.filterEmployeeDataWith(string: "Ce") {
+            let firstSearchedResult = EmployeeViewModel.filteredEmployeesList[0]
+            XCTAssertEqual(firstSearchedResult.employeeID, "4")
+            XCTAssertEqual(firstSearchedResult.employeeName, "Cedric Kelly")
+            XCTAssertEqual(firstSearchedResult.employeeAge, "22")
+            XCTAssertEqual(firstSearchedResult.employeeSalary, "433060")
+        }
+        
+    }
+    
+    func testEmployeeSearchZeroResult() {
+        employeeViewModel.filterEmployeeDataWith(string: "Rohit") {
+            XCTAssertEqual(EmployeeViewModel.filteredEmployeesList.count, 0)
+        }
+    }
+    
+    func testEmployeeDeleteSucessResponse() {
+        let expectation = self.expectation(description: "Delete Web Service response successful.")
+        employeeViewModel.deleteEmployeeFromList(forCellNumber: 0, IsFilteringOn: false, completion: {result in
+            switch(result) {
+            case .success(let result):
+                XCTAssertNotNil(result)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            expectation.fulfill()
+        })
+        self.waitForExpectations(timeout: 7, handler: nil)
+    }
+    
 }
